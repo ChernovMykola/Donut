a72e69912423256e49ccaba9cfc6cf2ba90aff28
ChernovMykola <4ernovnikola@gmail.com>
2023-04-24 21:07:04 +0200

    UpdateModel

--- donut_app/models.py
+++ donut_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Donut(models.Model):
     DONUT_LABEL_CHOICES = (
@@ -19,4 +20,7 @@ class Donut(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("donut:donut:detail", kwargs={'pk': self.pk})
 
--- donut_app/views.py
+++ donut_app/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import(
     ListView,
-    CreateView,
-    UpdateView,
-    DeleteView,
     DetailView,
     TemplateView,
 )
@@ -12,9 +9,11 @@ from donut_app.models import Donut
 class DonutListView(ListView):
     model = Donut
     template_name = 'donut/home.html'
+    context_object_name = 'donut'
 
 class DonutDetailView(DetailView):
     model = Donut
+    template_name = 'donut/donut_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
