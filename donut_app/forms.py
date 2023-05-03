67d3e96fd000e8af2e6992ee34dd8d67098e5c4b
ChernovMykola <4ernovnikola@gmail.com>
2023-05-03 12:24:13 +0200

    CreateOrderForm

--- donut_app/forms.py
+++ donut_app/forms.py
@@ -1,14 +1,14 @@
-from django import form
+from django import forms
 from donut_app.models import (
     Donut,
     Order,
     OrderItem
 )
 
-# class OrderCreate(forms.ModelForm):
-#     class Meta():
-#         model = Order
-#         fields = ('customer_name', 'customer_email','customer_address', 'items', 'total_price', 'created_at', 'updated_at')
-#         widgets = {
-#
-#         }
+class OrderCreate(forms.ModelForm):
+    class Meta():
+        model = Order
+        fields = ('customer_name', 'customer_email','customer_address', 'items', 'total_price', 'created_at', 'updated_at')
+        widgets = {
+
+        }
