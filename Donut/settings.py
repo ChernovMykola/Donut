eca7d84d28d64bfbb689cfd6f2c0a0d5c3016af7
ChernovMykola <4ernovnikola@gmail.com>
2023-05-12 22:20:10 +0200

    Deploy

--- Donut/settings.py
+++ Donut/settings.py
@@ -85,12 +85,22 @@ WSGI_APPLICATION = 'Donut.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'donut',
+#         'USER': 'mykolachernov',
+#         'PASSWORD': '/password',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'donut',
+        'NAME': 'donut_nsje',
         'USER': 'mykolachernov',
-        'PASSWORD': '/password',
+        'PASSWORD': 'OpQyfncqc2MdM7W7NgPaPR7e06ItEqz8',
+        'HOST': 'dpg-chf2vue4dad1jqdqop4g-a.oregon-postgres.render.com',
         'PORT': '5432',
     }
 }
